aedcb55f0e18a2f0d28ca974adb5c5754d203944
Simeon Olorunninwo <simeontoluwanimi@gmail.com>
2019-05-14 16:12:08 +0100

    new update: setters and getters for two attributes

--- Book.java
+++ Book.java
@@ -1,10 +1,21 @@
 
 public class Book { //create class book
     
-    private int noOfPages;
-    private String author;
-    private String genre;
-    private String coverType;
+    private int noOfPages; // attribute number of pages
+    private String author; // attribute author
+    private String genre; // attribute genre of book
+    private String coverType; // attribute type of bookcover
 
-
-}
+    public void setNoOfPages(int noOfPages) { // setter for attribute number of pages
+        this.noOfPages = noOfPages;
+    }
+    public int getNoOfPages() { // getter for attribute number of pages
+        return noOfPages;
+    }
+    public void setAuthor(String author) { // setter for attribute author
+        this.author = author;
+    }
+    public String getAuthor() { // getter for attribute author
+        return author;
+    }
+} // end class Book
